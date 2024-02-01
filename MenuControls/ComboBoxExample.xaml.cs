70199a7a220fbec8a931289a6f0748681dd15c25
guruf <guru@VS2017EE-PC>
2024-02-01 14:54:32 +0530

    Updated

---
+++ MenuControls/ComboBoxExample.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MenuControls
+{
+    /// <summary>
+    /// Interaction logic for ComboBoxExample.xaml
+    /// </summary>
+    public partial class ComboBoxExample : Window
+    {
+        public ComboBoxExample()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            MessageBox.Show("Yo seletes" + " " + Comb1.SelectedItem.ToString());
+        }
+    }
+}
---
+++ MenuControls/TreeViewExample.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MenuControls
+{
+    /// <summary>
+    /// Interaction logic for TreeViewExample.xaml
+    /// </summary>
+    public partial class TreeViewExample : Window
+    {
+        public TreeViewExample()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem item = new TreeViewItem();
+            item.Header=txtTreeSet.Text;
+            FruitTreeSet.Items.Add(item);
+        }
+
+        
+    }
+}
